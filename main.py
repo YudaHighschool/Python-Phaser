e4cf1e5da1989a86f74989f3791535bf02e6787f
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-02 16:34:56 +0800

    m

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Game(object):
 
     def preload(self, *args):
         this = javascript.this()
-        this.load.setBaseURL = 'http://labs.phaser.io'  
+        this.load.setBaseURL = 'http://labs.phaser.io/'  
 
         this.load.image('sky', 'https://images.unsplash.com/photo-1474573892045-721452c3d98c?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=8a1c8e04786fad38cae94ee48ce372e7')
         this.load.image('logo', 'https://yd-main.web.app/assets/favicon/android-chrome-192x192.png')
