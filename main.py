b05264dee346eaf28a2b8208417d93779e6b9aa6
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-02 16:41:26 +0800

    m

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ class Game(object):
         this = javascript.this()
         # this.load.setBaseURL = 'http://labs.phaser.io/'  
 
-        this.load.image('sky', 'assets/yuda-background.jpg')
+        this.load.image('sky', 'assets/yuda-background2.jpg')
         this.load.image('logo', 'assets/logo.svg')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.image('red', 'assets/logo.svg')
 
     def create(self, *args):
         this = javascript.this()
